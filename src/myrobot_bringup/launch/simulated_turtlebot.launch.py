70e467b6ec52430436ed1fc0de18dfcd0ed84cdd
Janez <janez@cime.si>
2025-06-03 12:30:44 +0200

    made myrobot_bringup package

---
+++ src/myrobot_bringup/launch/simulated_turtlebot.launch.py
@@ -0,0 +1,30 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    gazebo_robot_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+        os.path.join(get_package_share_directory('myrobot_gazebo'),
+            'launch',
+            'robot.launch.py')
+        )
+    )
+
+    cmd_timeout_node =  Node(
+        package='myrobot_nav',
+        executable='cmd_vel_timeout',
+        name='cmd_vel_timeout',
+        output='screen')
+
+    return LaunchDescription([
+        gazebo_robot_launch,
+        cmd_timeout_node
+    ])
